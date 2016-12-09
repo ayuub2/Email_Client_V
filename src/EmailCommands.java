e7a8e3e2c00586ce2f2de0e86240d7359873d0db
Ayuub Adan <ayuubadan@outlook.com>
2016-12-09 17:01:40 +0000

    Decided to do commands using scanner for now
    Decided to do commands using scanners as they are the easiest way to
    quickly get user input

--- src/EmailCommands.java
+++ src/EmailCommands.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 /**
  * Created by ayuub on 09/12/2016.
  */
 public class EmailCommands {
+
+    static Scanner reader = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Would you like gmail or outlook");
+        String type = reader.nextLine();
+        System.out.println("Are you sending or receiving emails?");
+        String action = reader.nextLine();
+    }
 }
