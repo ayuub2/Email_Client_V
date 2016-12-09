31f199174556ac13adc174340697c83f2de92ac1
Ayuub Adan <ayuubadan@outlook.com>
2016-12-09 17:04:38 +0000

    Created Second Class
    Created second class which will do most of the work of retreiving the
    emails and sending the emails. Currently i have designed the constructor
    to require a username passsword and type of email address.

---
+++ src/EmailClient.java
@@ -0,0 +1,14 @@
+/**
+ * Created by ayuub on 09/12/2016.
+ */
+public class EmailClient {
+    private String username;
+    private String password;
+    private String type;
+
+    public EmailClient(String username, String password, String type) {
+        this.username = username;
+        this.password = password;
+        this.type = type;
+    }
+}
